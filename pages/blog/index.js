4c5fdd12d89ee334bc6aa31ef82006d8b921aef1
shu00011 <koha.koishi.gnu.16@gmail.com>
2023-05-02 16:48:36 +0900

    feature: p.69 Linkタグの中にaタグは記述しない？

---
+++ pages/blog/index.js
@@ -0,0 +1,11 @@
+import Hero from 'components/hero'
+
+export default function Blog() {
+
+  return (
+    <Hero 
+        title="Blog"
+        subtitle="Recent Posts"    
+    />
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import Hero from 'components/hero'
+
 export default function Home() {
-  const subtitle = 'アウトプットしていくサイト'
 
   return (
-    <div className="hero">
-      <h1 style={{color: 'red', fontSize: '80px'}}>shu00011</h1>
-      <hr />
-      <p>{subtitle}</p>
-    </div>
+    <Hero 
+      title="shu00011"
+      subtitle="アウトプットしていくサイト"
+      imageOn
+    />
   )
 }
